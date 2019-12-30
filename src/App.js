de9304654d0af554aa89991cebb7b6b7c6940f4a
向井寛人 <mukaihiroto@mukaihiroto-no-MacBook-Pro.local>
2019-12-30 13:15:00 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import React, { useState } from 'react'
 
-const App  = () => {
+const App  = props => {
 
-  // useStateの第二引数は関数
-  const [initCount, setCount] = useState(0)
-
-  const increment = () => setCount(initCount + 1)
-  const decrement = () => setCount(initCount - 1)
-  
-  // useStateの第二引数の関数は引数を持つことができる
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  
-  const reset = () => setCount(0)
-  const double = () => setCount(initCount * 2)
-
-  const devide3 = () => setCount( previousCount => 
-    previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  )
-
-  // {}はobjectを表現しているので,返り値にはreturnが必要となる。
-  // const devide3 = () => setCount(previousCount => {
-  //   return previousCount %3 === 0 ? previousCount / 3 : previousCount
-  // })
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  const reset = () => {
+    setName(props.name)
+    setPrice(props.price)
+  }
 
   return (
     <React.Fragment>
-      {/* <React.Fragment> は <> のこと */}
-      <div>count:{initCount}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={double}>Double</button>
-        <button onClick={devide3}>divide by 3</button>
-        <button onClick={reset}>Reset</button>
-      </div>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <input value={name} onChange={e=> setName(e.target.value)}/>
+      <button onClick={reset}>Reset</button>
     </React.Fragment>
   );
+}
+
+App.defaultProps = {
+  name: 'サンプル',
+  price: 1000
 }
 
 export default App;
