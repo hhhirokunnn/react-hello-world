d6b51d64beb024f92bdba260868b2539e2d6ebaa
向井寛人 <mukaihiroto@hiro-pc.local>
2020-01-05 19:10:08 +0900

    save state with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from './EventForm.js'
 import reducer from '../reducers'
@@ -6,13 +6,23 @@ import Events from './Events'
 import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 
+const APP_KEY = 'appWithRedux'
+
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY)
+
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
+
   // refs: https://ja.reactjs.org/docs/hooks-reference.html#usereducer
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(()=>{
+    localStorage.setItem(APP_KEY, JSON.stringify(state))
+  }, [state])
+  
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className= "container-fluid">
